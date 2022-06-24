30d1178fc1e7d624a930608990a216910e782138
Guillermo Bescos <guibescos>
2022-06-24 10:50:23 +0000

    Add ts files

--- tests/pyth-example.ts
+++ tests/pyth-example.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { PythExample } from "../target/types/pyth_example";
+import { PublicKey} from "@solana/web3.js";
 
 describe("pyth-example", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,9 @@ describe("pyth-example", () => {
 
   const program = anchor.workspace.PythExample as Program<PythExample>;
 
-  it("Is initialized!", async () => {
+  it("Get BTC price", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.getPrice().accounts({price: new PublicKey("HovQMDrbAgAYPCmHVSrezcSmkMtXSSUsLDFANExrZh2J")}).rpc();
     console.log("Your transaction signature", tx);
   });
 });
